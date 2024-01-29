ebef206bc8d0b7e61a7b02826591b27b0f3615e6
Sathiyanarayanan G <84400233+G-SathiyaNarayanan@users.noreply.github.com>
2024-01-29 16:25:42 +0530

    Hangman Game

--- Pr-3: Hangman Game.py
+++ Pr-3: Hangman Game.py
@@ -1,4 +1,4 @@
-'''The 1st  method uses only if, if-else, for, while loops and random functions but 2nd method by chat GPT use some other functions also so that it looks pretty than my code.
+'''The 1st method uses only if, if-else, for, while loops and random functions but 2nd method by chat GPT use some other functions also so that it looks pretty than my code.
 * I done this base upon learning of mainly on While loop and Random Function *
 
 Project Description:
